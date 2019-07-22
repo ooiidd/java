a6dc77503010047c3fc52fafa4989100f175e264
SungHun <sunghun.hong@lgcns.com>
2019-07-22 09:34:11 +0900

    초기화

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("홍성훈",33));
+        list.add(new Person("홍성충",31));
+        list.add(new Person("헤헤헤",43));
+        list.add(new Person("가나다",53));
+        list.add(new Person("홍가나",23));
+
+
+        System.out.println("");
+    }
+}
